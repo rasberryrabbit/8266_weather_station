5a9255fa4bc4538564607b732232c2e5e40284fc
rasberryrabbit <lovelust@hanmail.net>
2019-12-22 07:24:02 +0900

    Fix wifi connection.

--- apconn.lua
+++ apconn.lua
@@ -3,7 +3,7 @@ station_cfg.ssid=""
 station_cfg.pwd=""
 
 conntry=15
-aptried={}
+aptry={}
 connectionMode=true
 
 conntmr=tmr.create()
@@ -11,47 +11,74 @@ conntmr=tmr.create()
 function listap(t)
   for ssid,v in pairs(t) do
     local authmode, _, _, _ = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
-    if (not aptried[ssid]) and authmode=="0" then
-      aptried[ssid]=0
-      wifi.sta.disconnect()
-      wifi.setmode(wifi.STATION)
-      station_cfg.ssid=ssid
-      station_cfg.pwd=""
-      wifi.sta.config(station_cfg)
-      wifi.sta.connect()
-      conntmr:start()
-      conntry=15
-      break
-    end
-  end
-  if conntry==0 then
-    wifi.sta.disconnect()
-    wifi.setmode(wifi.STATIONAP)
-    wifi.ap.config({ssid="Weather_"..node.chipid(), auth=wifi.OPEN})
-    enduser_setup.manual(true)
-    enduser_setup.start(
-      function()
-        print("WiFi as:" .. wifi.sta.getip())
-        --node.restart()
-      end,
-      function(err, str)
-        print("Err #" .. err .. ": " .. str)
-      end
-    )
-    if connectionMode then
-      MsgSystem("No Internet.")
-      reboottmr=tmr.create()
-      reboottmr:register(300000,tmr.ALARM_SINGLE,function()
-        node.restart()
-      end)
-      reboottmr:start()
+    if aptry[ssid]==nil and authmode=="0" then
+      aptry[ssid]=0
     end
   end
 end
 
+conntmr:register(2000,tmr.ALARM_AUTO,function()
+    if wifi.sta.getip() == nil then
+      MsgSystem("IP unavailable, Wait")
+      conntry=conntry-1
+      if conntry==0 then
+        conntmr:stop()
+        
+        wifi.sta.getap(0,listap)
+        for ssid, id in pairs(aptry) do
+          if id==0 then
+            aptry[ssid]=1
+            wifi.sta.disconnect()
+            wifi.setmode(wifi.STATION)
+            station_cfg.ssid=ssid
+            station_cfg.pwd=""
+            wifi.sta.config(station_cfg)
+            wifi.sta.connect()
+            conntry=15
+            conntmr:start()
+            break
+          end
+        end
+        -- no network
+        if conntry==0 then
+          aptry={}
+          wifi.sta.disconnect()
+          wifi.setmode(wifi.STATIONAP)
+          wifi.ap.config({ssid="Weather_"..node.chipid(), auth=wifi.OPEN})
+          enduser_setup.manual(true)
+          enduser_setup.start(
+            function()
+              print("WiFi as:" .. wifi.sta.getip())
+              --node.restart()
+            end,
+            function(err, str)
+              print("Err #" .. err .. ": " .. str)
+            end
+          )
+          if connectionMode then
+            MsgSystem("No Internet.")
+            reboottmr=tmr.create()
+            reboottmr:register(300000,tmr.ALARM_SINGLE,function()
+              node.restart()
+            end)
+            reboottmr:start()
+          end
+        end
+      end
+    else
+      conntmr:stop()
+      aptry={}
+      print("MAC: " .. wifi.ap.getmac())
+      MsgSystem("IP: "..wifi.sta.getip())
+
+      pcall(function() sntp.sync(nil,nil,nil,1) end)
+      _G.gotip=true
+    end
+end)
+
 function doWiFiConnect(reconnect)
     conntry=15
-    aptried={}
+    aptry={}
     connectionMode=reconnect
     _G.gotip=false
 
@@ -66,24 +93,6 @@ function doWiFiConnect(reconnect)
     wifi.sta.config(station_cfg)
     wifi.sta.connect()
 
-    conntmr:register(2000,tmr.ALARM_AUTO,function()
-        if wifi.sta.getip() == nil then
-            MsgSystem("IP unavailable, Wait")
-            conntry=conntry-1
-            if conntry==0 then
-              conntmr:stop()
-              
-              wifi.sta.getap(0,listap)
-            end
-        else
-            conntmr:unregister()
-            print("MAC: " .. wifi.ap.getmac())
-            MsgSystem("IP: "..wifi.sta.getip())
-
-            pcall(function() sntp.sync(nil,nil,nil,1) end)
-            _G.gotip=true
-        end
-    end)
     conntmr:start()
 end
 
