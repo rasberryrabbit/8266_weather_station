10d0069a85bf78f330a3904d4275c9c7bff603d6
rasberryrabbit <lovelust@hanmail.net>
2021-03-16 14:25:22 +0900

    Use local.

--- xbmconv.lua
+++ xbmconv.lua
@@ -1,24 +1,24 @@
-XBMList = {'we_01d.xbm','we_01n.xbm','we_02d.xbm','we_02n.xbm','we_03d.xbm','we_03n.xbm','we_04d.xbm','we_04n.xbm',
+local XBMList = {'we_01d.xbm','we_01n.xbm','we_02d.xbm','we_02n.xbm','we_03d.xbm','we_03n.xbm','we_04d.xbm','we_04n.xbm',
            'we_09d.xbm','we_09n.xbm','we_10d.xbm','we_10n.xbm','we_11d.xbm','we_11n.xbm','we_13d.xbm','we_13n.xbm',
 		   'we_50d.xbm','we_50n.xbm'}
 
-function ConvXBM(w,h,str)
+local function ConvXBM(w,h,str)
   if file.exists(str) then
-    nf=string.gsub(str,".xbm",".bin")
+    local nf=string.gsub(str,".xbm",".bin")
     if file.exists(nf) then
 	  return
 	end
-    fo=file.open(nf,"w")
-    local obuf=""
+    local fo=file.open(nf,"w")
     local bpl=math.ceil(w/8)
+	local obuf=""
     local xx=0
     local yy=0
     local buf
-    f=file.open(str,"r")
+    local f=file.open(str,"r")
     buf=f:readline()
     while buf~=nil do
       for wv in string.gmatch(buf,"0x[^%s,]+") do
-        v=bit.band(bit.bnot(tonumber(wv,16)),0xff)
+        local v=bit.band(bit.bnot(tonumber(wv,16)),0xff)
         obuf=obuf..string.char(v)
         xx=xx+1
         if xx>=bpl then
@@ -41,7 +41,7 @@ function ConvXBM(w,h,str)
 end
 
 print("Convert XBM Files")
-MsgSystem("Convert XBM Files")
+pcall(function() MsgSystem("Convert XBM Files") end)
 for k,v in pairs(XBMList) do
   ConvXBM(32,32,v)
 end
